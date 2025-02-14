91f250f264f4fe4fcb0da0d540e0adbe9c79b638
Jefferson Barbosa <jeffersonbsousa@gmail.com>
2025-02-13 22:58:33 -0300

    finalização da navbar

---
+++ sc-fogoes/src/app/components/navbar.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image"
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <div className="w-full bg-blue-950 text-white">
+            <div className="max-w-screen-2xl mx-auto flex justify-evenly uppercase font-bold items-center py-2 text-xs sm:text-base">
+                <Link href="/">
+                    <Image
+                        priority
+                        src="/img/ScFogoes.png"
+                        alt="Sc fogoes logo"
+                        width={240}
+                        height={200}
+                        className="cursor-pointer w-24 sm:w-60"
+                    />
+                </Link>
+                <Link href="/" className="hover:text-yellow-200 transition-colors">
+                    Inicio
+                </Link>
+                <Link href="/servicos" className="hover:text-yellow-200 transition-colors">
+                    Serviços
+                </Link>
+                <Link href="/contato" className="hover:text-yellow-200 transition-colors">
+                    Contato
+                </Link>
+                <Link href="/sobre" className="hover:text-yellow-200 transition-colors">
+                    Sobre
+                </Link>
+            </div>
+        </div>
+    )
+}
