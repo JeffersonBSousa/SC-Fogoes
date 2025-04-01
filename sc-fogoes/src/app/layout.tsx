48da27cd9c88f206d0d708b6f4cad45e8484673c
Jefferson Barbosa <jeffersonbsousa@gmail.com>
2025-03-31 22:51:01 -0300

    att

--- sc-fogoes/src/app/layout.tsx
+++ sc-fogoes/src/app/layout.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "SC Fogões",
+  description: "Empresa especializada em instalação e manutenção de fogões",
+};
 
 export default function RootLayout({
   children,
